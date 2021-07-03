036b86d060666f376ea4d1d6a56319ffa67419ac
MarioG8 <golikmariusz7@gmail.com>
2021-07-03 20:04:04 +0200

    project initialed added new files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is  listening at http://localhost:${port}`);
+});
---
+++ controllers/tasks.js
@@ -0,0 +1,24 @@
+const getAllTasks = (req, res) => {
+  res.send("all items from hell");
+};
+
+const createTask = (req, res) => {
+  res.send("create task");
+};
+const getTask = (req, res) => {
+  res.send("get single task");
+};
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
