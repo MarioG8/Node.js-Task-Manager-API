5d8a7cf407da6012711ab57bf783f70d3d44249d
MarioG8 <golikmariusz7@gmail.com>
2021-07-05 23:53:41 +0200

    added delete and update controllers

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -33,11 +33,38 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `This task with id: ${taskID} does not exist!` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
-const deleteTask = (req, res) => {
-  res.send("delete task");
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `This task with id: ${taskID} does not exist!` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
