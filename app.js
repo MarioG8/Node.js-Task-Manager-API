6b5fe7bc14549c68662be02c8ffa0b3ee64254b5
MarioG8 <golikmariusz7@gmail.com>
2021-07-13 08:50:03 +0200

    added port settings, ready for heroku

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use("/api/v1/tasks", tasks);
 app.use(errorHandlerMiddleware);
 app.use(notFound);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
