385edf839a26661781ed96d6795b8a8343dad06d
MarioG8 <golikmariusz7@gmail.com>
2021-07-13 08:40:06 +0200

    added custom error handling

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,5 +1,6 @@
 const Task = require("../models/Task");
 const asyncWrapper = require("../middleware/async");
+const { createCustomError, CustomAPIError } = require("../errors/custom-error");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -11,13 +12,13 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return res
-      .status(404)
-      .json({ msg: `This task with id: ${taskID} does not exist!` });
+    return next(
+      createCustomError(`This task with id: ${taskID} does not exist!`, 404)
+    );
   }
   res.status(200).json({ task });
 });
@@ -26,9 +27,10 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res
-      .status(404)
-      .json({ msg: `This task with id: ${taskID} does not exist!` });
+    return createCustomError(
+      `This task with id: ${taskID} does not exist!`,
+      404
+    );
   }
   res.status(200).json({ task });
 });
@@ -40,9 +42,10 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
-    return res
-      .status(404)
-      .json({ msg: `This task with id: ${taskID} does not exist!` });
+    return createCustomError(
+      `This task with id: ${taskID} does not exist!`,
+      404
+    );
   }
   res.status(200).json({ task });
 });
